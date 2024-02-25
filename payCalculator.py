5086c754923a6fedff2d3f54c50d2bd0ca4f4095
PatrickPau <patrickpauchiho@gmail.com>
2024-02-25 14:13:37 +1100

    first commit

---
+++ payCalculator.py
@@ -0,0 +1,9 @@
+oneHundredNote = float(input("enter 100 note: "))
+fiftyNote = float(input("enter 50 note: "))
+twentyNote = float(input("enter 20 note: "))
+tenNote = float(input("enter 10 note: "))
+fiveNote = float(input("enter 5 note: "))
+
+total_value = (100 * oneHundredNote) + (50 * fiftyNote) + (20 * twentyNote) + (10 * tenNote) + (5 * fiveNote)
+
+print("Total value of the notes: ", total_value)
